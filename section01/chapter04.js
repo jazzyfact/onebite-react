ef094cdbaf52f492cd53a5d31210cd3e88a2ff70
jazzyfact <jazzyfact2000@gmail.com>
2024-07-11 16:07:45 +0900

    [ADD} section01 js 추가

---
+++ section01/chapter04.js
@@ -0,0 +1,32 @@
+//1.변수
+let age = 27;
+consloe.log(age);
+
+
+//2.상수
+const birth = “1192.01.03”;
+birth = “123”;
+//변하지 않아야 할 값
+//초기화 값 필요함
+
+//3.변수 명명규칙(네이밍 규칙)
+//3-1. $,_  제외한 기호는 사용할 수 없다.
+//let name#
+
+//3-2. 숫자로 시작할 수 없다.
+let name1;
+let nam2e;
+
+//3-3.예약어를 사용할 수 없다.
+//let let if 
+
+//4. 변수 명명 가이드
+//let a = 1;
+//let b = 2’
+//let c = a- b;
+
+let saleCount = 1;
+let refundCount = 2;
+let totalSalesCount = salesCount - refuntCount
+
+//변수는 언제든지 값을 바꿀수있게 때문에 선언할 때 초기 값을 이런식으로 설정해주지 않아도 된다
